e81ce68ebb4e1306229b3621d6152e0348717b41
AWGilbert <austingilbertaw@gmail.com>
2021-09-15 10:33:55 -0500

    methods for constructors, getters, and setters

--- BackendTweetAnalysis/backendtweetanalysis/Tweet.java
+++ BackendTweetAnalysis/backendtweetanalysis/Tweet.java
@@ -6,11 +6,52 @@ public class Tweet {
 	private String username;
 	private String text;
 	
+	//
 	public Tweet() {
 		id = (Integer) null;
 		polarity = (Integer) null;
 		username = "USER UNKNOWN";
 		text = "NO TWEET TEXT";
+	}
+	
+	public Tweet(int id, int polarity, String user, String text) {
+		this.id = id;
+		this.polarity = polarity;
+		user = username;
+		this.text = text;
+	}
+	
+	//Generate the getters and Setters for the private data members
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getPolarity() {
+		return polarity;
+	}
+
+	public void setPolarity(int polarity) {
+		this.polarity = polarity;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
 	}
 
 }
--- BackendTweetAnalysis/backendtweetanalysis/TweetCollection.java
+++ BackendTweetAnalysis/backendtweetanalysis/TweetCollection.java
@@ -4,5 +4,45 @@ import java.util.ArrayList;
 
 public class TweetCollection {
 	private ArrayList<Tweet> myData;
+	private int size;
+	private String filename;
+	
+	
+	public TweetCollection() {
+		
+	}
+
+
+	//Generate the getters and setters for the data fields
+	public ArrayList<Tweet> getMyData() {
+		return myData;
+	}
+
+
+	public void setMyData(ArrayList<Tweet> myData) {
+		this.myData = myData;
+	}
+
+
+	public int getSize() {
+		return size;
+	}
+
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+
+	public String getFilename() {
+		return filename;
+	}
+
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	
+	
 	
 }
