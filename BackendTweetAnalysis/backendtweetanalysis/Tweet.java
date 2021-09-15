288b42a29b9631ab53933961c1be748e1ea3231f
AWGilbert <austingilbertaw@gmail.com>
2021-09-15 10:05:28 -0500

    1st commit

---
+++ BackendTweetAnalysis/backendtweetanalysis/Tweet.java
@@ -0,0 +1,16 @@
+package backendtweetanalysis;
+
+public class Tweet {
+	private int id;
+	private int polarity;
+	private String user;
+	private String text;
+	
+	public Tweet() {
+		id = (Integer) null;
+		polarity = (Integer) null;
+		user = "Unnamed";
+		text = ""
+	}
+
+}
---
+++ BackendTweetAnalysis/backendtweetanalysis/TweetCollection.java
@@ -0,0 +1,5 @@
+package backendtweetanalysis;
+
+public class TweetCollection {
+
+}
